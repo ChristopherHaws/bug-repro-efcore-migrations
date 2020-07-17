20d28f7aeccf682a0623af1c1fef3b5f5a0799ac
Christopher Haws <Christopher.Haws@arvato.com>
2020-07-16 21:49:51 -0700

    Add bad migration

---
+++ EFCoreMigrationUpdateData/Migrations/20200717044918_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreMigrationUpdateData.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestEntities",
+                columns: table => new
+                {
+                    Code = table.Column<string>(nullable: false),
+                    TimeZone = table.Column<string>(nullable: false, defaultValueSql: "('UTC')")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestEntities", x => x.Code)
+                        .Annotation("SqlServer:Clustered", true);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TestEntities",
+                columns: new[] { "Code", "TimeZone" },
+                values: new object[] { "code-1", "Eastern Standard Time" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestEntities");
+        }
+    }
+}
---
+++ EFCoreMigrationUpdateData/Migrations/20200717044931_NoChanges.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCoreMigrationUpdateData.Migrations
+{
+    public partial class NoChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TestEntities",
+                keyColumn: "Code",
+                keyValue: "code-1",
+                column: "TimeZone",
+                value: "Eastern Standard Time");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TestEntities",
+                keyColumn: "Code",
+                keyValue: "code-1",
+                column: "TimeZone",
+                value: "Eastern Standard Time");
+        }
+    }
+}
