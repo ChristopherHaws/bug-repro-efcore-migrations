39d8eb0601ebb4c97b3b5c5d1d0b129bc3975efc
Christopher Haws <Christopher.Haws@arvato.com>
2020-07-16 22:10:30 -0700

    Attempt in .NET 5

--- EFCoreMigrationUpdateData/Migrations/20200717050550_NoChanges2.Designer.cs
+++ EFCoreMigrationUpdateData/Migrations/20200717050550_NoChanges2.Designer.cs
@@ -3,14 +3,16 @@ using EFCoreMigrationUpdateData;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EFCoreMigrationUpdateData.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200717050550_NoChanges2")]
+    partial class NoChanges2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
